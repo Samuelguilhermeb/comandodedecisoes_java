b5b64dc40fdfeaed76a7d3646273bab0f1d18b3e
Samuelguilhermeb <99374269+Samuelguilhermeb@users.noreply.github.com>
2022-03-14 13:30:14 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+       int pos=1;
+
+       switch(pos){
+           case 1:
+               System.out.println("Primeiro Lugar");
+               break;
+           case 2:
+               System.out.println("Segundo Lugar");
+               break;
+           case 3:
+               System.out.println("Terceiro Lugar");
+               break;
+           default:
+               System.out.println("Não ganhou");
+               break;
+
+       
+       }
+    }     
+}    
+
